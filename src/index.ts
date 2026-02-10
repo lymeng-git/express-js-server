00d556906e47ee6057f923703d598c85b02f6419
lymeng-git <henglimengee@gmail.com>
2026-02-10 23:55:29 +0700

    connect with front-end REACT

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express,{Request,Response} from 'express';
 import usersRouter from './routes/users';
 import staffRouter from './routes/staff';
+import cors from 'cors';
+import morgan from 'morgan';
+import compression from 'compression';
 
 const app = express();
+app.use(
+    cors({
+        origin:"http://127.0.0.1:5173",
+        methods:["GET","POST","PUT","DELETE"],
+        credentials:true,
+    })
+);
 app.use(express.json());
+app.disable("x-powered-by");
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(compression());
+
 
 app.use('/api/user',usersRouter);
 app.use('/staff',staffRouter);
