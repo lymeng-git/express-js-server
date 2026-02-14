b69e57a5f4883de504d662fe5ad7e570afe40d61
lymeng-git <henglimengee@gmail.com>
2026-02-14 17:15:03 +0700

    add GET filter

--- src/handlers/salary.ts
+++ src/handlers/salary.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express-serve-static-core";
 import pool from "../db/db";
 import { Salary, SalaryResponse, Staff } from '../types/response';
+import { error } from "node:console";
 
 export const getMonthlySalary = async (req: Request, res: Response) => {
+    const { month, year } = req.query;
     try {
         const result_salary = await pool.query(`
             SELECT *
             FROM salary_table
             INNER JOIN ubet24_staff
-            ON ubet24_staff.staffid = salary_table.staffid;
+            ON ubet24_staff.staffid = salary_table.staffid
+            WHERE year=${year} AND month=${month}
         `);
-        res.status(200).json(result_salary.rows);
+        if (result_salary.rowCount === 0) {
+            res.status(404).json({error:"Data not found in Database."});
+        } else {
+            res.status(200).json(result_salary.rows);
+        }
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Database error' });
